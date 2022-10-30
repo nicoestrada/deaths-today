1e45c906d62061b2c4aafbab5dd61e47fa41faec
Nico <estradanicolas@gmail.com>
2022-10-30 13:44:40 -0500

    added month, day and deaths of the day from wiki

--- scrape.js
+++ scrape.js
@@ -5,20 +5,28 @@ const fs = require('fs')
 //const writeStream = fs.createWriteStream(`${moment.defaultFormat}.html`);
 
 const YESTERDAY = moment().subtract(1, 'day')
-const currMonth = YESTERDAY.format('MMMM')
+const currMonth = YESTERDAY.format('MMMM');
 
 request(`https://en.wikipedia.org/wiki/Deaths_in_2022#${YESTERDAY}`, (error,
 response, html) => {
     if (!error && response.statusCode == 200) {
         const $ = cheerio.load(html);
+        const day = $('h3').first().next().text()
+        const deaths = $('h3').first().next().next('ul').text()
 
-        $('.mw-parser-output').each((i, el) => {
-            const currDay = $(el)
-                .find('.mw-headline')
-                .text();
+        console.log(
+            currMonth,
+            day,'\n',
+            deaths
+        )
+
+        // $('.mw-parser-output').each((i, el) => {
+        //     const currDay = $('ul').children().first().text();
+        //         //.find('.mw-headline')
+        //         //.text();
    
-            console.log(currDay);
-        })
+        //     console.log(currDay);
+        // })
 
         // const mwParserOutput = $('.mw-parser-output');
         // console.log(mwParserOutput
