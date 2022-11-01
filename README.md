74f99790c945687196387a12a4840373d83fada2
Nico <estradanicolas@gmail.com>
2022-11-01 10:32:22 -0500

    recalibrated request, organized output to generate new dir and html file

--- README.md
+++ README.md
@@ -1,6 +1,15 @@
 # Notable Deaths of the Day
 
+From Wikipedia's real-time notable deaths page. Data consists of name, age, country of citizenship at birth, subsequent nationality (if applicable), what subject the person was noted for, cause of death and reference.
+
+For more information, click [here](https://en.wikipedia.org/wiki/Deaths_in_2022).
+
 ## Components:
+
+Please see below a log of current progress and future tasks:
 
  * [x] [Parse deaths and date](https://github.com/nicoestrada/deaths-today/commit/1e45c906d62061b2c4aafbab5dd61e47fa41faec)
  * [x] [Build HTML file with data](https://github.com/nicoestrada/deaths-today/commit/d56174f33c90d7462ef49d4641b32684eb9ffac9)
+ * [x] Recalibrate the request of deaths to be a complete list of total from yesterday
+ * [x] Established parsing output to generate a new html file in 2 new public directories
+ * [ ] Integrate responsive HTML/CSS layout to output
--- main.js
+++ main.js
@@ -6,18 +6,42 @@ const fs = require('fs')
 const URL = 'https://en.wikipedia.org/wiki/Deaths_in_2022'
 const yesterday = moment().subtract(1, 'day')
 const currMonth = yesterday.format('MMMM')
-const currDay = moment().format('D')
+const currDay = yesterday.format('D')
 
-const writeStream = fs.createWriteStream(`${moment().format('MMMM_Do_YYYY')}.html`);
+//html template
+const template = (content) => {
+    return `
+  <!doctype html>
+  <html>
+    <head>
+    </head>
+    <body>
+        ${content}
+    </body>
+  </html>`
+  }
+//end template
 
-request(URL, (error, response, html) => {
-    if (!error && response.statusCode == 200) {
-        const $ = cheerio.load(html);
-        const deaths = $('h3').first().next().next().next('ul').text()
+const makeDir = (folderPath) => {
+    return new Promise((resolve, reject) => {
+        fs.mkdir(folderPath, { recursive: true}, (err) => {
+            if (err) return reject(err)
+            else return resolve(true)
+        })
+    })
+}
 
-        writeStream.write(`${currMonth} ${currDay}, ${moment().format('YYYY')} \n ${deaths}`)
-    } else {
-        throw new Error('Something is not working right', error)
-    }
-    console.log("Success...check the root folder for the file")
+//logic
+request(URL, async (error, response, body) => {
+    if (error) throw new Error('Something is not working right', error)
+
+    const $ = cheerio.load(body);
+    const deaths = $('h3').first().next().next().next().next().next('ul').text()
+
+    const dir = `./public/${yesterday.format('YYYY/MMMM')}`
+    const filename = `${yesterday.format('D')}.html`
+    let created = await makeDir(dir);
+    fs.writeFileSync(`${dir}/${filename}`, template(deaths))
+
+    console.log("Success...check the root folder")
 })
