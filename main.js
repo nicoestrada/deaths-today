e428f302bff5e9e17742957e919173f28c59c553
Nico <estradanicolas@gmail.com>
2023-02-24 11:32:04 -0600

    added footer notes

--- main.js
+++ main.js
@@ -151,6 +151,24 @@ const template = (content) => {
               <tr>
                 <td class="wrapper" style="font-family: sans-serif; font-size: 14px; vertical-align: top; box-sizing: border-box; padding: 20px;">
                   <table border="0" cellpadding="0" cellspacing="0" style="border-collapse: separate; mso-table-lspace: 0pt; mso-table-rspace: 0pt; width: 100%;">
+                  <tr>
+                  <td style="font-family: sans-serif; font-size: 14px; vertical-align: top;">
+                    ----
+                  </td>
+                </tr>
+                <tr>
+                  <td style="font-family:'Avenir','Helvetica Neue',Helvetica,'Lucida Grande',sans-serif; font-size: 14px; vertical-align: top;">
+                    Made by <a href="https://twitter.com/stradamoney" style="text-decoration: none; color: ${linkColor}; ">Nico Estrada</a>.
+                  </td>
+                </tr>
+                <tr>
+                  <td style="font-family:'Avenir','Helvetica Neue',Helvetica,'Lucida Grande',sans-serif; font-size: 14px; vertical-align: top;">
+                    Missed a day? <a href="https://deaths.today/${moment(currDay)
+                      .subtract(1, 'days')
+                      .format('MMMM DD, YYYY')
+                      .toLowerCase()}" style="text-decoration: none; color:${linkColor};">See day before → </a>
+                  </td>
+                </tr>
                   </table>
                 </td>
               </tr>
