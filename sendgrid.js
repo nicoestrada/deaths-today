b7cce1a2c54eaa27377c5a83e491e7c5bedc787f
Nico <estradanicolas@gmail.com>
2023-02-24 12:40:00 -0600

    sendgrid setup

--- sendgrid.js
+++ sendgrid.js
@@ -7,26 +7,32 @@ const html = fs.readFileSync(__dirname + '/public/index.html', 'utf8')
 const sgMail = require("@sendgrid/mail");
 
 sgMail.setApiKey(`${process.env.SENDGRID_API_KEY}`)
-const sendMail = async (msg) => {
-    try {
-        await sgMail.send(msg);
-        console.log("Message sent successfully.")
-    } catch (error) {
-        console.error(error);
 
-        if (error.response) {
-            console.log(error.response.body);
+request(
+    {
+        method: 'GET',
+        url: 'https://api.sendgrid.com/v3/marketing/contacts',
+        headers: { authorization: `Bearer ${process.env.SENDGRID_API_KEY}` },
+    },
+    function (error, response, body) {
+        if (error) throw new Error(error)
+        let json = JSON.parse(body)
+        let { result: contacts } = json
+        let personalizations = contacts.map((x) => ({
+          to: {
+            email: x.email,
+          },
+        }))
+        const msg = {
+            personalizations,
+            from: {
+              email: 'no-reply@deaths.today',
+              name: `Notable Deaths`,
+            },
+            subject: `Notable Deaths: ${today.format('dddd DD MMMM YYYY')}`,
+            text: 'Text not available. See latest current events at https://deaths.today',
+            html,
+          }
+          sgMail.send(msg)
         }
-    }
-};
-
-sendMail({
-    to: "estradanicolas@gmail.com",
-    from: {
-        email: 'no-reply@deaths.today',
-        name: `Deaths Today`,
-      },
-    subject: `Deaths Today: ${today.format('dddd MMMM DD YYYY')}`,
-    text: 'Text not available. See who died at https://deaths.today',
-    html,
-})
+    )
