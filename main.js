87775eb2eb38b244af2e567ce94af0e2d5692373
Nico <estradanicolas@gmail.com>
2023-02-24 11:11:53 -0600

    edited parse

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ const cheerio = require('cheerio')
 const moment = require('moment-timezone')
 const fs = require('fs')
 
-const URL = 'https://en.wikipedia.org/wiki/Deaths_in_2022'
+const URL = 'https://en.wikipedia.org/wiki/Deaths_in_2023'
 moment().tz('America/Chicago').format("MMMM DD, YYYY")
 const yesterday = moment().subtract(1, 'day')
 const currMonth = yesterday.format('MMMM')
@@ -207,7 +207,7 @@ request(URL, async (error, response, body) => {
   cleanseLinks($)
   removeRefs($)
 
-  const deaths = $('h3').first().next().next().next().next('ul').html()
+  const deaths = $('h3').next().next().next().next().next('ul').html()
   const dir = `./public/${yesterday.format('YYYY/MMMM')}`
   const filename = `${yesterday.format('D')}.html`
   let created = await makeDir(dir);
