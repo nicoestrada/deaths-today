6da090210d016dbe93e7b5e65bc106e23c1728ed
Nico <estradanicolas@gmail.com>
2023-06-22 12:47:10 -0500

    sendgrid configs

--- sendgrid.js
+++ sendgrid.js
@@ -1,38 +1,18 @@
-require("dotenv").config();
-var today = moment().subtract(1, 'day')
-const fs = require('fs')
-var request = require('request')
-const moment = require('moment')
-const html = fs.readFileSync(__dirname + '/public/index.html', 'utf8')
-const sgMail = require("@sendgrid/mail");
+const sgMail = require('@sendgrid/mail')
+sgMail.setApiKey(process.env.SENDGRID_API_KEY)
 
-sgMail.setApiKey(`${process.env.SENDGRID_API_KEY}`)
-
-request(
-    {
-        method: 'GET',
-        url: 'https://api.sendgrid.com/v3/marketing/contacts',
-        headers: { authorization: `Bearer ${process.env.SENDGRID_API_KEY}` },
-    },
-    function (error, response, body) {
-        if (error) throw new Error(error)
-        let json = JSON.parse(body)
-        let { result: contacts } = json
-        let personalizations = contacts.map((x) => ({
-          to: {
-            email: x.email,
-          },
-        }))
-        const msg = {
-            personalizations,
-            from: {
-              email: 'no-reply@deaths.today',
-              name: `Notable Deaths`,
-            },
-            subject: `Notable Deaths: ${today.format('dddd DD MMMM YYYY')}`,
-            text: 'Text not available. See latest current events at https://deaths.today',
-            html,
-          }
-          sgMail.send(msg)
-        }
-    )
+const msg = {
+    to: 'estradanicolas@gmail.com', // Change to your recipient
+    from: 'no-reply@deaths.today', // Change to your verified sender
+    subject: 'Sending with SendGrid is Fun',
+    text: 'and easy to do anywhere, even with Node.js',
+    html: '<strong>and easy to do anywhere, even with Node.js</strong>',
+  }
+  sgMail
+    .send(msg)
+    .then(() => {
+      console.log('**********Email sent')
+    })
+    .catch((error) => {
+      console.error(error)
+    })
