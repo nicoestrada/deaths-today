d3d644ffdc5eb5625cf1e759d4253e1360e6a757
Nico <59927633+nicoestrada@users.noreply.github.com>
2023-11-10 12:40:03 -0600

    new

--- sendgrid.js
+++ sendgrid.js
@@ -7,13 +7,15 @@ const html = fs.readFileSync(__dirname + '/public/index.html', 'utf8')
 var today = moment().subtract(1, 'day')
 require('dotenv').config();
 sgMail.setApiKey(`${process.env.SENDGRID_API_KEY}`)
+//new below
+const client = require('@sendgrid/client');
+client.setApiKey(`${process.env.SENDGRID_API_KEY}`);
 
-request(
-  {
-    method: 'POST',
-    url: 'https://api.sendgrid.com/v3/marketing/contacts/search/emails',
-    headers: { authorization: `Bearer ${process.env.SENDGRID_API_KEY}` },
-  },
+
+const request = {
+  url: "https://api.sendgrid.com/v3/marketing/contacts",
+  methods: 'GET',
+  headers: { authorization: `Bearer ${process.env.SENDGRID_API_KEY}` },
   function (error, response, body) {
     if (error) throw new Error(error)
     let json = JSON.parse(body)
@@ -24,7 +26,6 @@ request(
         email: x.email,
       },
     }))
-    
     const msg = {
       personalizations,
       from: {
@@ -43,5 +44,43 @@ request(
       .catch((error) => {
         console.error(error)
       })
-  }
-)
+}}
+
+
+// request(
+//   {
+//     method: 'GET',
+//     url: 'https://api.sendgrid.com/v3/marketing/contacts',
+//     headers: { authorization: `Bearer ${process.env.SENDGRID_API_KEY}` },
+//   },
+//   function (error, response, body) {
+//     if (error) throw new Error(error)
+//     let json = JSON.parse(body)
+//     let { result: contacts } = json
+//     let personalizations = 
+//       contacts?.map((x) => ({
+//       to: {
+//         email: x.email,
+//       },
+//     }))
+    
+//     const msg = {
+//       personalizations,
+//       from: {
+//         email: 'no-reply@deaths.today',
+//         name: `Notable Deaths Today`,
+//       },
+//       subject: `Notable Deaths: ${yesterday.format('MMMM DD, YYYY')}`,
+//       text: 'See latest notable deaths at https://deaths.today',
+//       html,
+//     }
+//     sgMail
+//       .send(msg)
+//       .then(() => {
+//         console.log('**********Email sent')
+//       })
+//       .catch((error) => {
+//         console.error(error)
+//       })
+//   }
+// )
